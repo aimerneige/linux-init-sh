1e1d75647eef18022d8dd756a895aa3442b33602
Aimer Neige <aimer.neige.soft@gmail.com>
2021-09-15 17:53:49 +0800

    fix

--- install-icalingua.sh
+++ install-icalingua.sh
@@ -1,6 +1,4 @@
-sudo
 icalingua_latest_download_link=$(curl -s https://api.github.com/repos/Clansty/Icalingua/releases/latest | grep browser_download_url | grep x86_64.rpm | cut -d '"' -f 4)
-icalingua_rpm_save_location=$HOME/Downloads/Icalingua.rpm
-aria2c -s16 -x16 -k1M -o ${icalingua_rpm_save_location} ${icalingua_latest_download_link}
-sudo dnf install ${icalingua_rpm_save_location}
-rm ${icalingua_rpm_save_location}
+aria2c -s16 -x16 -k1M -d $HOME/Downloads/InitScriptTemp -o Icalingua.rpm ${icalingua_latest_download_link}
+sudo dnf install $HOME/Downloads/InitScriptTemp/Icalingua.rpm
+rm ${HOME}/Downloads/Icalingua.rpm
