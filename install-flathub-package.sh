342123d0cd488d82b54415dd65bff283d7aff9ea
Aimer Neige <aimer.neige.soft@gmail.com>
2021-09-17 18:13:54 +0800

    update set -e

---
+++ install-flathub-package.sh
@@ -0,0 +1,33 @@
+set -e
+
+sudo flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+
+flatpak install flathub com.spotify.Client -y
+flatpak install flathub net.cozic.joplin_desktop -y
+flatpak install flathub com.github.mdh34.quickdocs -y
+flatpak install flathub com.getpostman.Postman -y
+flatpak install flathub com.obsproject.Studio -y
+flatpak install flathub io.github.shiftey.Desktop -y
+flatpak install flathub io.typora.Typora -y
+flatpak install flathub com.discordapp.Discord -y
+flatpak install flathub org.telegram.desktop -y
+flatpak install flathub org.videolan.VLC -y
+flatpak install flathub io.mpv.Mpv -y
+flatpak install flathub info.smplayer.SMPlayer -y
+flatpak install flathub org.kde.kdenlive -y
+flatpak install flathub com.belmoussaoui.Decoder -y
+flatpak install flathub com.diy_fever.DIYLayoutCreator -y
+flatpak install flathub com.github.junrrein.PDFSlicer -y
+flatpak install flathub net.codeindustry.MasterPDFEditor -y
+flatpak install flathub com.github.alecaddd.sequeler -y
+flatpak install flathub io.dbeaver.DBeaverCommunity -y
+flatpak install flathub com.github.gi_lom.dialect -y
+flatpak install flathub com.github.johnfactotum.Foliate -y
+flatpak install flathub com.github.robertsanseries.ciano -y
+flatpak install flathub com.leinardi.gst -y
+flatpak install flathub io.gitlab.librewolf-community -y
+flatpak install flathub net.meijn.onvifviewer -y
+flatpak install flathub de.haeckerfelix.Shortwave -y
+flatpak install flathub ch.openboard.OpenBoard -y
+flatpak install flathub io.github.hakuneko.HakuNeko -y
+flatpak install flathub org.shotcut.Shotcut -y
