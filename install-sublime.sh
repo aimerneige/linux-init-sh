5a721ec349693793aede38a80d9942739299c766
Aimer Neige <aimer.neige.soft@gmail.com>
2022-03-30 21:33:28 +0800

    update install-sublime.sh

--- install-sublime.sh
+++ install-sublime.sh
@@ -1,6 +1,11 @@
 set -e
 
-sudo rpm -v --import https://download.sublimetext.com/sublimehq-rpm-pub.gpg
-sudo dnf config-manager --add-repo https://download.sublimetext.com/rpm/stable/x86_64/sublime-text.repo
-sudo dnf update
-sudo dnf install sublime-text
+if [[ $EUID != 0 ]]; then
+    echo "You must run this script with `sudo`."
+    exit 1
+fi
+
+rpm -v --import https://download.sublimetext.com/sublimehq-rpm-pub.gpg
+dnf config-manager --add-repo https://download.sublimetext.com/rpm/stable/x86_64/sublime-text.repo
+dnf update
+dnf install sublime-text
