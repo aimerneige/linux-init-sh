46eef714eedd31371883e2d2495247badd36d42e
Aimer Neige <aimer.neige.soft@gmail.com>
2022-03-30 22:43:23 +0800

    update

--- install-flathub-package.sh
+++ install-flathub-package.sh
@@ -2,72 +2,138 @@ set -e
 
 sudo flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 
+# spotify
 flatpak install flathub com.spotify.Client -y
+# joplin
 flatpak install flathub net.cozic.joplin_desktop -y
-flatpak install flathub com.github.mdh34.quickdocs -y
+# postman
 flatpak install flathub com.getpostman.Postman -y
+# obs
 flatpak install flathub com.obsproject.Studio -y
+# github desktop
 flatpak install flathub io.github.shiftey.Desktop -y
+# typora
 flatpak install flathub io.typora.Typora -y
+# discord
 flatpak install flathub com.discordapp.Discord -y
+# telegram desktop
 flatpak install flathub org.telegram.desktop -y
+# vlc
 flatpak install flathub org.videolan.VLC -y
+# mpv
 flatpak install flathub io.mpv.Mpv -y
+# smplayer
 flatpak install flathub info.smplayer.SMPlayer -y
+# kdenlive
 flatpak install flathub org.kde.kdenlive -y
+# decoder
 flatpak install flathub com.belmoussaoui.Decoder -y
+# DIY Layout Creator
 flatpak install flathub com.diy_fever.DIYLayoutCreator -y
+# PDF Slicer
 flatpak install flathub com.github.junrrein.PDFSlicer -y
+# Master PDF Editor
 flatpak install flathub net.codeindustry.MasterPDFEditor -y
+# Sequeler
 flatpak install flathub com.github.alecaddd.sequeler -y
+# DBeaver Community
 flatpak install flathub io.dbeaver.DBeaverCommunity -y
+# Dialect
 flatpak install flathub com.github.gi_lom.dialect -y
+# Foliate
 flatpak install flathub com.github.johnfactotum.Foliate -y
+# Ciano
 flatpak install flathub com.github.robertsanseries.ciano -y
+# GtkStressTesting
 flatpak install flathub com.leinardi.gst -y
+# LibreWolf
 flatpak install flathub io.gitlab.librewolf-community -y
+# ONVIFViewer
 flatpak install flathub net.meijn.onvifviewer -y
+# Shortwave
 flatpak install flathub de.haeckerfelix.Shortwave -y
+# OpenBoard
 flatpak install flathub ch.openboard.OpenBoard -y
+# HakuNeko
 flatpak install flathub io.github.hakuneko.HakuNeko -y
+# Shotcut
 flatpak install flathub org.shotcut.Shotcut -y
+# Android Studio
 flatpak install flathub com.google.AndroidStudio -y
+# Remmina
 flatpak install flathub org.remmina.Remmina -y
+# Okular
 flatpak install flathub org.kde.okular -y
+# Krita
 flatpak install flathub org.kde.krita -y
+# GIMP
 flatpak install flathub org.gimp.GIMP -y
+# Blender
 flatpak install flathub org.blender.Blender -y
+# Flameshot
 flatpak install flathub org.flameshot.Flameshot -y
+# Libre Office
 flatpak install flathub org.libreoffice.LibreOffice -y
+# ONLYOFFICE Desktop Editors
 flatpak install flathub org.onlyoffice.desktopeditors -y
+# qBittorrent
 flatpak install flathub org.qbittorrent.qBittorrent -y
+# Calibre
 flatpak install flathub com.calibre_ebook.calibre -y
+# Audio Sharing
 flatpak install flathub de.haeckerfelix.AudioSharing -y
+# Apostrophe
 flatpak install flathub org.gnome.gitlab.somas.Apostrophe -y
+# Fedora MediaWriter
 flatpak install flathub org.fedoraproject.MediaWriter -y
+# Blanket
 flatpak install flathub com.rafaelmardojai.Blanket -y
+# Cozy
 flatpak install flathub com.github.geigi.cozy -y
+# Collision
 flatpak install flathub dev.geopjr.Collision -y
+# Drawing
 flatpak install flathub com.github.maoschanz.drawing -y
+# Fragments
 flatpak install flathub de.haeckerfelix.Fragments -y
+# Font Downloader
 flatpak install flathub org.gustavoperedo.FontDownloader -y
+# Wike
 flatpak install flathub com.github.hugolabe.Wike -y
+# Tangram
 flatpak install flathub re.sonny.Tangram -y
+# VideoTrimmer
 flatpak install flathub org.gnome.gitlab.YaLTeR.VideoTrimmer -y
+# Webfont Kit Generator
 flatpak install flathub com.rafaelmardojai.WebfontKitGenerator -y
+# Text Pieces
 flatpak install flathub com.github.liferooter.textpieces -y
+# Solanum
 flatpak install flathub org.gnome.Solanum -y
-flatpak install flathub de.haeckerfelix.Shortwave -y
+# Share Preview
 flatpak install flathub com.rafaelmardojai.SharePreview -y
+# Plots
 flatpak install flathub com.github.alexhuntley.Plots -y
+# Obfuscate
 flatpak install flathub com.belmoussaoui.Obfuscate -y
+# NewsFlash
 flatpak install flathub com.gitlab.newsflash -y
+# Mousai
 flatpak install flathub io.github.seadve.Mousai -y
+# Drumstick Metronome
 flatpak install flathub com.adrienplazas.Metronome -y
+# Metadata Cleaner
 flatpak install flathub fr.romainvigier.MetadataCleaner -y
+# Khronos
 flatpak install flathub io.github.lainsce.Khronos -y
+# Kooha
 flatpak install flathub io.github.seadve.Kooha -y
+# Gnome Connections
 flatpak install flathub org.gnome.Connections -y
+# Gnome Boxes
 flatpak install flathub org.gnome.Boxes -y
+# Sound Recorder
 flatpak install flathub org.gnome.SoundRecorder -y
+# GHex
 flatpak install flathub org.gnome.GHex -y
+
