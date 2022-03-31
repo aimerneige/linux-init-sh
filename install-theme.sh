671948d40df5a0d4a9a028d9fcf75f99e40eb1c4
Aimer Neige <aimer.neige.soft@gmail.com>
2022-03-31 15:44:55 +0800

    update

--- install-theme.sh
+++ install-theme.sh
@@ -3,21 +3,21 @@ set -e
 echo "Start clone Mojave-gtk-theme"
 git clone https://github.com/vinceliuice/Mojave-gtk-theme.git /tmp/Mojave-gtk-theme
 echo "Start install Mojave-gtk-theme"
-/tmp/Mojave-gtk-theme/install.sh
+sudo /tmp/Mojave-gtk-theme/install.sh
 echo "Remove temp file"
 rm /tmp/Mojave-gtk-theme -rf
 
 echo "Start clone McMojave-circle"
 git clone https://github.com/vinceliuice/McMojave-circle.git /tmp/McMojave-circle
 echo "Start install McMojave-circle"
-/tmp/McMojave-circle/install.sh
+sudo /tmp/McMojave-circle/install.sh
 echo "Remove temp file"
 rm /tmp/McMojave-circle -rf
 
 echo "Start clone WhiteSur-gtk-theme"
 git clone https://github.com/vinceliuice/WhiteSur-gtk-theme.git /tmp/WhiteSur-gtk-theme
 echo "Start install WhiteSur-gtk-theme"
-/tmp/WhiteSur-gtk-theme/install.sh
+sudo /tmp/WhiteSur-gtk-theme/install.sh
 echo "Remove temp file"
 rm /tmp/WhiteSur-gtk-theme -rf
 
@@ -27,3 +27,11 @@ echo "Start install WhiteSur-icon-theme"
 /tmp/WhiteSur-icon-theme/install.sh
 echo "Remove temp file"
 rm /tmp/WhiteSur-icon-theme -rf
+
+echo "Start clone gruvbox-material-gtk"
+git clone https://github.com/TheGreatMcPain/gruvbox-material-gtk /tmp/gruvbox-material-gtk
+echo "Start install gruvbox-material-gtk"
+sudo cp /tmp/gruvbox-material-gtk/themes/* /usr/share/themes -y
+sudo cp /tmp/gruvbox-material-gtk/icons/* /usr/share/icons -y
+echo "Remove temp file"
+rm /tmp/gruvbox-material-gtk -rf
