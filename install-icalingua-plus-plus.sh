cd4214c0f5f612817e727560fe6c93910a10b1b3
Aimer Neige <aimer.neige.soft@gmail.com>
2022-03-31 15:25:41 +0800

    update

--- install-icalingua-plus-plus.sh
+++ install-icalingua-plus-plus.sh
@@ -1,6 +1,5 @@
 set -e
 icalingua_latest_download_link=$(curl -s https://api.github.com/repos/Icalingua-plus-plus/Icalingua-plus-plus/releases/latest | grep browser_download_url | grep x86_64.rpm | cut -d '"' -f 4)
-mkdir -p $HOME/Downloads/InitScriptTemp/
-aria2c -s16 -x16 -k1M -d $HOME/Downloads/InitScriptTemp -o Icalingua.rpm ${icalingua_latest_download_link}
-sudo dnf install $HOME/Downloads/InitScriptTemp/Icalingua.rpm
-rm ${HOME}/Downloads/InitScriptTemp -rf
+aria2c -s16 -x16 -k1M -d /tmp/Icalingua -o Icalingua.rpm ${icalingua_latest_download_link}
+sudo dnf install /tmp/Icalingua/Icalingua.rpm
+rm /tmp/Icalingua -rf
