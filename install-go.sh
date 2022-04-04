17cff6aea9787276faec91999048c1d6af4ada71
Aimer Neige <aimer.neige.soft@gmail.com>
2022-04-04 20:07:42 +0800

    update install go

--- install-go.sh
+++ install-go.sh
@@ -2,3 +2,4 @@ set -e
 
 wget https://go.dev/dl/go1.18.linux-amd64.tar.gz -O /tmp/go1.18.linux-amd64.tar.gz
 sudo rm -rf /usr/local/go && sudo tar -C /usr/local -xzf /tmp/go1.18.linux-amd64.tar.gz
+rm /tmp/go1.18.linux-amd64.tar.gz
