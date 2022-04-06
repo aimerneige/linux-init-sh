fce4523b30c31b6e4179c59b684dacb6f65fcac2
AimerNeige <aimer.neige.soft@gmail.com>
2022-04-06 14:10:31 +0800

    fix

--- install-arduino-1.8.19.sh
+++ install-arduino-1.8.19.sh
@@ -1,6 +1,5 @@
 set -e
 wget https://downloads.arduino.cc/arduino-1.8.19-linux64.tar.xz -O /tmp/Arduino-1.8.19.tar.xz
-mkdir -p /tmp/Arduino-1.8.19
-tar -xvf /tmp/Arduino-1.8.19.tar.xz -C /tmp/Arduino-1.8.19
-sudo /tmp/Arduino-1.8.19/arduino-1.8.19/install.sh
-rm /tmp/Arduino-1.8.19 -rf
+sudo tar -xvf /tmp/Arduino-1.8.19.tar.xz -C /opt
+sudo /opt/arduino-1.8.19/install.sh
+rm /tmp/Arduino-1.8.19.tar.xz
