219fb645734db708721ba6eb321d737eb463fc16
Aimer Neige <aimer.neige.soft@gmail.com>
2022-04-07 21:41:20 +0800

    update

--- install-nodejs.sh
+++ install-nodejs.sh
@@ -1,8 +1,8 @@
 set -e
 
-NODE_VERSION=v16.14.2
-NODE_DISTRO=linux-x64
-NODE_ROOT=/usr/local/lib/nodejs
+NODE_VERSION="v16.14.2"
+NODE_DISTRO="linux-x64"
+NODE_ROOT="/usr/local/lib/nodejs"
 
 wget https://nodejs.org/dist/$(NODE_VERSION)/node-$(NODE_VERSION)-$(NODE_DISTRO).tar.xz -O /tmp/node-$(NODE_VERSION)-$(NODE_DISTRO).tar.xz
 # TODO
